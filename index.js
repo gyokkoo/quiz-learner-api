af46a0a83e798ebd91c7c2fde506d95d101b91a8
Gyokan <gyokan.nezhdetov@gmail.com>
2018-03-06 17:43:49 +0200

    Removed server folder | Moved files

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const env = process.env.NODE_ENV || 'development'
 
 // Setup MongoDB connection
-let settings = require('./server/config/settings')[env]
-require('./server/config/database')(settings)
+let settings = require('./config/settings')[env]
+require('./config/database')(settings)
 
 let app = express()
-require('./server/config/express')(app)
+require('./config/express')(app)
 
 // Import server-routes
-require('./server/config/routes')(app)
+require('./config/routes')(app)
 
 // Setup MongoDB connection
-require('./server/config/passport')()
+require('./config/passport')()
 
 app.listen(settings.port, () => console.log(`Node.js server running on port ${settings.port}...`))
