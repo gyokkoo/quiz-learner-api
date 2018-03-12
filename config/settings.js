f1c47b5fd568e93ef457586256d21dddcd588cbd
Gyokan <gyokan.nezhdetov@gmail.com>
2018-03-12 18:54:50 +0200

    Deployment changes

--- config/settings.js
+++ config/settings.js
@@ -6,7 +6,8 @@ let port = process.env.PORT || 8080
 module.exports = {
   development: {
     rootPath: rootPath,
-    db: 'mongodb://localhost:27017/quiz-learner',
+   // db: 'mongodb://localhost:27017/quiz-learner',
+    db: 'mongodb://gyoko:GFnZoiH6W7bD@ds149138.mlab.com:49138/net-shop',
     port: port
   },
   staging: {
